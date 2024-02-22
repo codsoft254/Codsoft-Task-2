89414dd89b6c577425d6c2c90091dab33bc85cfc
stephen otieno <Stephenowin233@gmail.com>
2024-02-22 23:36:32 +0300

    prompting a user to re-enter instead of terminating the project

--- src/Main.java
+++ src/Main.java
@@ -17,70 +17,72 @@ public class Main {
 //        average percentage.
 //        Grade Calculation: Assign grades based on the average percentage achieved.
 //        Display Results: Show the total marks, average percentage, and the corresponding grade to the user
-
-
+        
+        
         //COD + SOFT TASK 2.
         int totalMarks = 0;
-
-
+        
         System.out.println("Enter marks obtained in the following subjects:");
-
-        //initializing an array of subjects
-        String[] subjects = {"Math", "Physics", "Biology", "Chemistry", "Agriculture", "Business Studies", "Computer Studies"};
-
+        
+        // initializing an array of subjects
+        String[] subjects = {"Math", "Physics", "Biology", "Chemistry", "Agriculture", "Business Studies",
+                "Computer Studies"};
+        
         for (int i = 0; i < subjects.length; i++) {
-            //accessing each subject in the array during iteration
-            System.out.print("Enter marks obtained in " + subjects[i] + ": ");
-            try {
-                int marks = scan.nextInt();
-                //if the input is less than or greater than 100,
-                //the loop will break
-                if (marks < 0 || marks >100){
-                    System.out.println("Please provide marks that are within the range of 1% to 100%.");
-                    break;
-
-                }else {
-
-                    totalMarks += marks;
+            // accessing each subject in the array during iteration
+            while (true) {
+                System.out.print("Enter marks obtained in " + subjects[i] + ": ");
+                try {
+                    int marks = scan.nextInt();
+                    // if the input is less than or greater than 100,
+                    // prompt the user to re-enter the input
+                    if (marks < 0 || marks > 100) {
+                        System.out.println("Please provide marks that are within the range of 1% to 100%.");
+                    } else {
+                        totalMarks += marks;
+                        break; // break the loop if valid input is provided
+                    }
+                    
+                } catch (Exception e) {
+                    // System.out.println(e.getMessage());
+                    System.out.println("Invalid Marks! Please enter a valid integer.");
+                    scan.next(); // consume the invalid input to avoid an infinite loop
                 }
-
-            } catch (Exception e) {
-                //System.out.println(e.getMessage());
-                System.out.println("Invalid Marks!");
-                break;
             }
-
         }
-
+        
+        //getting average
         double average = (double) totalMarks / subjects.length;
+        
+        //printing the results
         System.out.println("\nResults:");
         System.out.println("Total Marks obtained: " + totalMarks);
-
-        System.out.println("Average percentage = " + String.format( "%.2f",average) + "%");
-
-        //grading!
+        
+        //converting the average to two decimal places
+        System.out.println("Average percentage = " + String.format("%.2f", average) + "%");
+        
+        // grading!
         Grade grade;
-        if (average > 90){
+        if (average > 90) {
             grade = Grade.A;
-
+            
         } else if (average > 80) {
-            grade=Grade.B;
+            grade = Grade.B;
         } else if (average > 70) {
-            grade=Grade.C;
-
-        }else if (average > 60){
-            grade=Grade.D;
+            grade = Grade.C;
+            
+        } else if (average > 60) {
+            grade = Grade.D;
         } else if (average > 50) {
-            grade=Grade.E;
-
-        }else {
+            grade = Grade.E;
+            
+        } else {
             grade = Grade.F;
         }
         // Grade Display!
         System.out.println("Grade: " + grade);
-
-        //scanner closed to avoid memory leak!
+        
+        // scanner closed to avoid memory leak!
         scan.close();
-
     }
 }
